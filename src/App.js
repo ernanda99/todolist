baeace7e52e299f92284175bb8241774c7e469cd
ernanda <ernandasatria1997@gmail.com>
2022-06-22 23:06:44 +0700

    initial comit

---
+++ src/App.js
@@ -0,0 +1,116 @@
+import './App.css';
+import {useState} from 'react'
+
+function App() {
+  const [todoList,setTodoList] = useState([])
+  const [form, setForm] = useState({
+    todo: '',
+    status : false
+  })
+
+  const resetInput = () => {
+    setForm({
+       todo: '',
+       status : false
+    })
+  }
+
+  const handleChange = (e) =>{
+    setForm({
+      ...form,
+      todo: e.target.value,
+      status : false
+    })
+  }
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    if(form.index || form.index === 0){
+      const newTodo = todoList.map((e, i) => {
+        if (i === form.index) {
+          return form
+        }else{
+          return e
+        }
+      })
+     setTodoList(newTodo)
+    }else{setTodoList([
+      ...todoList,
+      form
+    ])
+
+    }
+    
+    resetInput()
+  }
+
+
+  const handleCheck = (index) => {
+    const newTodo = todoList.map((e, i) => {
+      if (i === index) {
+        return {
+          todo : e.todo,
+          status : true
+        }
+      }else{
+        return e
+      }
+    })
+    setTodoList(newTodo)
+   
+  }
+
+  const handleDelete = (index) => {
+    const newTodo = todoList.filter((e, i ) => {
+      if (i !== index) {
+        return e
+      }
+    })
+    setTodoList(newTodo)
+  }
+   
+  const handleEdit = (index) => {
+    const detailTodo = {
+      index,
+      ...todoList[index]
+    }
+    setForm(detailTodo)
+  }
+
+  return (
+    <div>
+      <div className= "jumbotroon">
+        <h1>Todo List APP </h1>
+       
+        <form className="form" method="post" onSubmit={handleSubmit}>
+          <input type="text" name="todo" value={form.todo} onChange={handleChange} placeholder="Todo ...."></input>
+          <button type="submit" className="btn-submit">Submit</button>
+        </form>
+      </div> 
+        <div className="content"> 
+
+         {
+          todoList.map((e, i) => (
+            <div className="card">
+            <div className="action">  
+              <input type="checkbox"></input>
+            </div>
+            <div className="text">
+              {e.todo}
+              </div>
+
+            <div className="button-action">
+              <button className = "btn-edit" onClick = {() => handleEdit(i)}>Edit</button>
+              <button className = "btn-delete" onClick={() => handleDelete(i)}>Delete</button>
+
+            </div>
+          </div>
+          ))
+         }
+      
+        </div>
+      </div>
+    
+  );
+}
+export default App;
